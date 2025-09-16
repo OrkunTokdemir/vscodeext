3d3cfa0a704fff0ae9eb283442a42cb2eab948a8
Ben Cho <ben.cho@qt.io>
2025-09-16 10:59:20 +0200

    qt-python: Introduce qt-python extension
    - Recognize `pyproject.toml` file.
    - Add 4 tasks: run, build, clean and deploy.
    - Support debugging session using `debugpy`.
    - Setup npm scripts similar to other extensions.
    
    Task-number: [VSCODEEXT-29](https://bugreports.qt.io/browse/VSCODEEXT-29)
    Task-number: [VSCODEEXT-30](https://bugreports.qt.io/browse/VSCODEEXT-30)

---
+++ qt-python/src/types.ts
@@ -0,0 +1,20 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only
+
+// export enum ToolId {
+//   Project = 'project'
+// }
+
+export enum TaskId {
+  Run,
+  Build,
+  Clean,
+  Deploy
+}
+
+export type ProjectToolAction = 'run' | 'build' | 'clean' | 'deploy';
+
+export interface PySideProjectInfo {
+  name: string;
+  files: string[];
+}
